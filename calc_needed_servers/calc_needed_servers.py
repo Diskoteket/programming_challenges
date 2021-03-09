e5308222146d169d99996dbf317fa5c47f33d466
Diskoteket <timwa@protonmail.com>
2021-03-09 13:53:33 +0100

    commenting calc_needed_servers.py

--- calc_needed_servers/calc_needed_servers.py
+++ calc_needed_servers/calc_needed_servers.py
@@ -1,6 +1,6 @@
 import sys
 import time
-
+# Define function for converting lists to strings
 def listToString(s):  
     str1 = ""  
 
@@ -8,21 +8,23 @@ def listToString(s):
         str1 += c 
 
     return str1  
-
+# Initiate variables
 entryList = []
 rangesList =[]
 
 neededServers = 1
 var0 = '0'
-
+# Read input
 entryList = sys.stdin.read().splitlines()
-
+# Pop the first entry in list
 pop = entryList.pop(0)
 maxRequests = (str(pop).split(" "))[1]
 
 length = (len(entryList))
 x = 0
+# Start parsing data
 while x < length:
+    # Class entries matching range 000000 - 999999
     if len(entryList[x]) <= 3:
         byCharacter = list(entryList[x])
         while len(byCharacter) <= 5:
@@ -35,6 +37,7 @@ while x < length:
         rangesList.append(rangeEntry)
         x += 1
 
+    # Class entries matching range 00x000 - 00x999
     elif len(entryList[x]) == 4:
         byCharacter = list(entryList[x])
         while len(byCharacter) <= 5:
@@ -47,6 +50,7 @@ while x < length:
         rangesList.append(rangeEntry)
         x += 1
 
+    # Class entries matching range 0yx000 - 0yx999
     elif len(entryList[x]) == 5:
         byCharacter = list(entryList[x])
         while len(byCharacter) <= 5:
@@ -59,6 +63,7 @@ while x < length:
         rangesList.append(rangeEntry)
         x += 1
 
+    # Class entries matching range zyx000 - zyx999
     elif len(entryList[x]) == 6:
         byCharacter = list(entryList[x])
         while len(byCharacter) <= 5:
@@ -71,8 +76,10 @@ while x < length:
         rangesList.append(rangeEntry)
         x += 1
 
+# Create a dictionary to count all entries matching range
 rangesDict = {i:rangesList.count(i) for i in rangesList}
 
+# Determine how many servers are needed by asking dictionary
 for key in rangesDict:
     if rangesDict[key] <= int(maxRequests):
         pass

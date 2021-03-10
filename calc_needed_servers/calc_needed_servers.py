7df88d187aa3203bc0c084c91bcfdc256b12beb5
Diskoteket <timwa@protonmail.com>
2021-03-10 16:21:40 +0100

    cleaning code of unused variables etc, commenting some more stuff

--- calc_needed_servers/calc_needed_servers.py
+++ calc_needed_servers/calc_needed_servers.py
@@ -1,36 +1,30 @@
 import sys
-import time
-# Define function for converting lists to strings
-def listToString(s):  
-    str1 = ""  
 
-    for c in s:  
-        str1 += c 
-
-    return str1  
 # Initiate variables
 entryList = []
 rangesList =[]
-
 neededServers = 1
-var0 = '0'
+
 # Read input
 entryList = sys.stdin.read().splitlines()
-# Pop the first entry in list
-pop = entryList.pop(0)
-maxRequests = (str(pop).split(" "))[1]
 
+# Pop the first entry in list and determine threshold of max requests per milisecond
+pop = entryList.pop(0)
+maxRequests = int((str(pop).split(" "))[1])
+
+# Variables for looping through input
 length = (len(entryList))
 x = 0
+
 # Start parsing data
 while x < length:
     # Class entries matching range 000000 - 000999
     if len(entryList[x]) <= 3:
+        # Read the length of the string, and insert 0 at position 0 of the list until it matches pattern
         byCharacter = list(entryList[x])
         while len(byCharacter) <= 5:
-            byCharacter.insert(0,var0)
-        startranges = (listToString(byCharacter))
-
+            byCharacter.insert(0,'0')
+        # Determine range of entry and append to list of x ranges
         rangeFrom = '000000'
         rangeTo = '000999'
         rangeEntry = "".join([rangeFrom,  rangeTo])
@@ -40,10 +34,10 @@ while x < length:
     # Class entries matching range 00x000 - 00x999
     elif len(entryList[x]) == 4:
         byCharacter = list(entryList[x])
+        # Read the length of the string, and insert 0 at position 0 of the list until it matches pattern
         while len(byCharacter) <= 5:
-            byCharacter.insert(0,var0)
-        startranges = (listToString(byCharacter))
-
+            byCharacter.insert(0,'0')
+        # Determine range of entry and append to list of x ranges
         rangeFrom = "".join(['00', str(int(entryList[x][0])), '000'])
         rangeTo = "".join(['00', str(int(entryList[x][0])), '999'])
         rangeEntry = "".join([rangeFrom,  rangeTo])
@@ -53,10 +47,10 @@ while x < length:
     # Class entries matching range 0yx000 - 0yx999
     elif len(entryList[x]) == 5:
         byCharacter = list(entryList[x])
+        # Read the length of the string, and insert 0 at position 0 of the list until it matches pattern
         while len(byCharacter) <= 5:
-            byCharacter.insert(0,var0)
-        startranges = (listToString(byCharacter))
-
+            byCharacter.insert(0,'0')
+        # Determine range of entry and append to list of x ranges
         rangeFrom = "".join(['0', str(int(entryList[x][0])), str(int(entryList[x][1])),'000'])
         rangeTo = "".join(['0', str(int(entryList[x][0])), str(int(entryList[x][1])),'999'])
         rangeEntry = "".join([rangeFrom,  rangeTo])
@@ -66,24 +60,26 @@ while x < length:
     # Class entries matching range zyx000 - zyx999
     elif len(entryList[x]) == 6:
         byCharacter = list(entryList[x])
+        # Read the length of the string, and insert 0 at position 0 of the list until it matches pattern
         while len(byCharacter) <= 5:
-            byCharacter.insert(0,var0)
-        startranges = (listToString(byCharacter))
-
+            byCharacter.insert(0,'0')
+        # Determine range of entry and append to list of x ranges
         rangeFrom = "".join([str(int(entryList[x][0])), str(int(entryList[x][1])), str(int(entryList[x][2])), '000'])
         rangeTo = "".join([str(int(entryList[x][0])), str(int(entryList[x][1])), str(int(entryList[x][2])), '999'])
         rangeEntry = "".join([rangeFrom,  rangeTo])
         rangesList.append(rangeEntry)
         x += 1
 
-# Create a dictionary to count all entries matching range
+# Create a dictionary to count all of ranges
 rangesDict = {i:rangesList.count(i) for i in rangesList}
 
-# Determine how many servers are needed by asking dictionary
+# Determine how many servers are needed by asking dictionary and calculate
 for key in rangesDict:
-    if rangesDict[key] <= int(maxRequests):
+    if rangesDict[key] <= maxRequests:
         pass
-    elif rangesDict[key] > int(maxRequests):
-        needed = (-(-rangesDict[key] // int(maxRequests)))
+    elif rangesDict[key] > maxRequests:
+        needed = (-(-rangesDict[key] // maxRequests))
         neededServers = neededServers + needed -1
+
+# Output servers needed
 sys.stdout.write(str(neededServers))
